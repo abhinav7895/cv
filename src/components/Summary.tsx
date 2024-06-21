cbbdfedefa4428bb190b75ec0cff91810bd7c5c6
abhinav7985 <abhinavay2003@gmail.com>
2024-06-21 14:06:38 +0530

    refactor: Update font weight in Summary component

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -4,7 +4,7 @@ import Section from './Section'
 const Summary = ({children} : {children : ReactNode}) => {
   return (
     <Section heading='Summary'>
-        <p className='pt-2 text-200 text-neutral-200 text-[15px] sm:text-base'>
+        <p className='pt-2 text-200 font-light text-neutral-200 text-[15px] sm:text-base'>
             {children}
         </p>
     </Section>
